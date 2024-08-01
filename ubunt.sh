77eb581bac54e8f6b06878e01068294921aea1b6
sasikaransk <89991289+sasikaransk@users.noreply.github.com>
2024-08-01 17:09:53 +0530

    Update ubunt.sh

--- ubunt.sh
+++ ubunt.sh
@@ -1,13 +1,13 @@
 sudo apt update
 sudo apt install snap
 sudo apt install snapd
-sudo apt install gnome
+sudo snap install firefox
 sudo DEBIAN_FRONTEND=noninteractive \apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
 sudo apt install screen
 sudo apt install -f
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install ./chrome-remote-desktop_current_amd64.deb
-wget https://go.microsoft.com/fwlink?linkid=2149051&brand=M102
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_127.0.2651.74-1_amd64.deb?brand=M102
 sudo dpkg -i microsoft-edge-stable_127.0.2651.74-1_amd64.deb
 wget https://github.com/TDesktop-x64/tdesktop/releases/download/v1.1.31/64Gram_1.1.31_linux.zip
 bash <(wget https://mkvtoolnix.download/appimage/MKVToolNix_GUI-86.0-x86_64.AppImage)
