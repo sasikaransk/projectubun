157ee347e4ad29fe30a91e1eeed6d9492d4a53ef
sasikaransk <89991289+sasikaransk@users.noreply.github.com>
2024-08-12 22:51:05 +0530

    Update ubunt.sh

--- ubunt.sh
+++ ubunt.sh
@@ -14,3 +14,12 @@ wget https://github.com/TDesktop-x64/tdesktop/releases/download/v1.1.31/64Gram_1
 bash <(wget https://mkvtoolnix.download/appimage/MKVToolNix_GUI-86.0-x86_64.AppImage)
 bash <(wget https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent-4.6.5_x86_64.AppImage)
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt update && sudo apt install firefox
