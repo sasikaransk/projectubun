bfc6c6226636f87ef6cfae06dc0ba3f5dfec9ad1
sasikaransk <89991289+sasikaransk@users.noreply.github.com>
2024-08-22 19:30:12 +0530

    Update ubunt.sh

--- ubunt.sh
+++ ubunt.sh
@@ -10,9 +10,10 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install ./chrome-remote-desktop_current_amd64.deb
 wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_127.0.2651.74-1_amd64.deb?brand=M102
 sudo dpkg -i microsoft-edge-stable_127.0.2651.74-1_amd64.deb
-wget https://github.com/TDesktop-x64/tdesktop/releases/download/v1.1.31/64Gram_1.1.31_linux.zip
-bash <(wget https://mkvtoolnix.download/appimage/MKVToolNix_GUI-86.0-x86_64.AppImage)
-bash <(wget https://www.fosshub.com/qBittorrent.html?dwl=qbittorrent-4.6.5_x86_64.AppImage)
+wget https://gitlab.com/a2622221/Sasirdp/-/raw/main/Telegram.AppImage
+sudo apt install mkvtoolnix mkvtoolnix-gui
+sudo apt install qbittorrent
+sudo apt install libqt5webenginecore5 libqt5webenginewidgets5 libqt5script5 libqt5scripttools5
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo install -d -m 0755 /etc/apt/keyrings
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
