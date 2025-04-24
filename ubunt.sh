6db674092fb095184a13d63827fddb3304a15f62
sasikaransk <89991289+sasikaransk@users.noreply.github.com>
2025-04-24 15:47:00 +0530

    Update ubunt.sh

--- ubunt.sh
+++ ubunt.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install snap
 sudo apt install snapd
 sudo snap install firefox
-#sudo apt install vlc -y
+sudo apt install vlc -y
 sudo apt install mpv
 #sudo snap install gnome-system-monitor
 sudo DEBIAN_FRONTEND=noninteractive \apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
